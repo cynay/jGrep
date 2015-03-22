b7a5a37efcad47d907a4a0d5e7b33585f0ecb595
Yannic Schneider <cynays@gmail.com>
2015-03-22 15:08:32 +0100

    Added Readme.md

--- jGrep/src/jGrep/Grep.java
+++ jGrep/src/jGrep/Grep.java
@@ -14,7 +14,7 @@ public class Grep {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printline("My grep...");
+		System.out.println("My grep...");
 	}
 
 }
