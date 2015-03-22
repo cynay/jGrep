6b763af4e2e028d2976d00e155fe1c4561648aa7
Yannic Schneider <cynays@gmail.com>
2015-03-22 14:46:30 +0100

    added Java Class Grep
    Signed-off-by: Yannic Schneider <cynays@gmail.com>

---
+++ jGrep/src/jGrep/Grep.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jGrep;
+
+/**
+ * @author cyn
+ *
+ */
+public class Grep {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printline("My grep...");
+	}
+
+}
