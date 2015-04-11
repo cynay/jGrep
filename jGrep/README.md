db332583dde4fd4a59d835c7fa8d9e3f767cdb7b
Yannic Schneider <cynays@gmail.com>
2015-04-11 13:33:55 +0200

    Refactor

--- jGrep/README.md
+++ jGrep/README.md
@@ -1,7 +1,24 @@
 jGrep [![icon](http://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Glider_shadow.svg/45px-Glider_shadow.svg.png)](http://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Glider_shadow.svg/45px-Glider_shadow.svg.png) 
 ========
 
-Java Implementation of the grep utility  
+Implementierung von Grep in Java
+
+grep ist ein Suchprogramm unter Unix. Die Eingabe besteht aus einem regulären 
+Ausdruck und einem oder mehreren Files. Das Programm druckt alle Zeilen der 
+angegebenen Files aus, die Teilstücke enthalten, die auf den angegebenen 
+regulären Ausdruck passen. 
+
+Hinweis (für eine Lösung mit Java):
+Um ein File zeilenweise einzulesen, kann man einen BufferedReader 
+(Package java.io) verwenden, zum Beispiel:
+
+BufferedReader bufferedReader = 
+    new BufferedReader(new FileReader(file));
+String line = bufferedReader.readLine();
+
+Zur Behandlung der regulären Ausdrücke sollten Sie die beiden Klassen Pattern 
+und Matcher aus dem Package java.util.regex verwenden. Lesen Sie die API 
+Dokumentationen dieser beiden Klassen. 
 
 features
 --------
@@ -12,9 +29,8 @@ features
 usage
 -----
 
-Some usage samples:
-
-    java jgrep ...
+grep regexp file 
+grep regexp file1 [file2] ...
 
 requirements
 ------------
@@ -29,7 +45,7 @@ install
 license
 -------
 
-Copyright (c) <2014> <cynays@gmail.com>
+Copyright (c) <2014> <cynay>
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--- jGrep/src/jGrep/Grep.java
+++ jGrep/src/jGrep/Grep.java
@@ -1,20 +1,162 @@
-/**
+/*
+ * Grep.java
+ * 
+ * Copyright 2015 cyn <cyn@vendetta>
+ * 
  * 
  */
 package jGrep;
 
-/**
- * @author cyn
- *
- */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.PatternSyntaxException;
+
+
 public class Grep {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("My grep...");
-	}
-
+    public static void main(String[] args){
+    	
+    	int argscount = args.length;
+    	
+    	if (argscount < 2){
+    		System.err.println("Usage: grep regex file");
+    		System.exit(1);
+    	}
+    	    	
+    	String regex = args[0];
+    	String file = args[1];
+    	
+    	System.out.println(argscount);
+    	System.out.println("---------");
+    	
+    	for(int x = 0; x < argscount; x++){
+    		System.out.println(args[x]);
+    	}
+    
+    	// Compile RE Pattern
+    	Pattern pat = null;
+    	try{
+    		pat = Pattern.compile(regex);
+    	}catch (PatternSyntaxException e) {
+    		System.err.println("Invalid RegulaExpression syntax: " 
+    				+ e.getDescription());
+    		System.exit(1);
+    	}
+    	
+    	// Read File
+    	BufferedReader in = null;
+    	try{
+    		in = new BufferedReader(new FileReader(file));
+    	}catch(FileNotFoundException e) {
+    		System.err.println("Unable to open file " + file 
+    				+ ": " + e.getMessage());
+    	}
+    	
+    	try {
+			String line;
+			while((line = in.readLine()) != null) {
+				Matcher m = pat.matcher(line);
+				if (m.find())
+					System.out.println(line);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+    }
 }
+
+
+
+/*
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.FileReader;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.regex.PatternSyntaxException;
+
+import org.kohsuke.args4j.*;
+
+public class Grep {
+
+    public static void main(String[] args){
+    	
+    	CommandLineValues values = new CommandLineValues(args);
+        CmdLineParser parser = new CmdLineParser(values);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.exit(1);
+        }
+
+        // Now you can use the command line values
+        
+    	
+    	
+    	
+    	
+    	String regex = args[0];
+    	String file = args[1];
+    	
+        Pattern pattern = null;
+        Matcher matcher = null;
+        //BufferedReader bf = new BufferedReader(new FileReader(file));
+        //String line = bf.readLine();
+        
+        System.out.println("miau");
+        
+        Console console = System.console();
+        if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+        while (true) {
+            try{
+                pattern = 
+                Pattern.compile(console.readLine("%nEnter your regex: "));
+
+                matcher = 
+                pattern.matcher(console.readLine("Enter input string to search: "));
+            }
+            catch(PatternSyntaxException pse){
+                console.format("There is a problem" +
+                               " with the regular expression!%n");
+                console.format("The pattern in question is: %s%n",
+                               pse.getPattern());
+                console.format("The description is: %s%n",
+                               pse.getDescription());
+                console.format("The message is: %s%n",
+                               pse.getMessage());
+                console.format("The index is: %s%n",
+                               pse.getIndex());
+                System.exit(0);
+            }
+            boolean found = false;
+            while (matcher.find()) {
+                console.format("I found the text" +
+                    " \"%s\" starting at " +
+                    "index %d and ending at index %d.%n",
+                    matcher.group(),
+                    matcher.start(),
+                    matcher.end());
+                found = true;
+            }
+            if(!found){
+                console.format("No match found.%n");
+            }
+        }
+    }
+}
+*/
+
+
